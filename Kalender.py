7583f7de349c58d9159eb673d4a503912acc2cdb
DeriArdiansyah <100026393+DeriArdiansyah@users.noreply.github.com>
2022-02-20 13:42:39 +0700

    Create Kalender.py

---
+++ Kalender.py
@@ -0,0 +1,6 @@
+#python program to print a calendar
+#of the specified month and year
+Import calendar
+Year = int(input ("enter year : "))
+Month = int(input ("enter month : "))
+Print ("\n", calendar.month(year,month))
