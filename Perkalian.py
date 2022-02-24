55dcbdc8b5ac2fcf3fc8287b99b6011f900dbb57
DeriArdiansyah <100026393+DeriArdiansyah@users.noreply.github.com>
2022-02-25 05:45:15 +0700

    Create Perkalian.py

---
+++ Perkalian.py
@@ -0,0 +1,10 @@
+n = 20
+For i in range(1, n+1):
+    Print ('\t' + str(i), end=' ')
+Print ()
+For j in range(1, n+1):
+    Print ('\j', end=('\t')
+    For k in range(1, n+1):
+        Print (j*k if j <= k else' ', end='\t')
+        If k % n == 0:
+           Print ()
